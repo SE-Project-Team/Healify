2ff01e8d9253426f9b8c6e7858b5f7dfe1c03616
roopasree123 <cs19b008@iittp.ac.in>
2021-09-30 13:44:07 +0530

    First commit

--- healify/src/App.js
+++ healify/src/App.js
@@ -15,9 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!!!
         </a>
-        <p>Dummy commit</p>
+        
       </header>
     </div>
   );
