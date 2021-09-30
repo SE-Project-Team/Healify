5e0850b42399a911298a086358fbfba2dbb2a85b
Githin George <home@pop-os.localdomain>
2021-09-30 23:05:36 +0530

    Generating Ideas

--- healify/src/App.js
+++ healify/src/App.js
@@ -1,11 +1,10 @@
-import logo from "./logo.svg";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import { Home } from "./Components/Home";
 function App() {
   return (
     <div className="App">
-      <p className="h1">Healify</p>
+      <Home />
     </div>
   );
 }
