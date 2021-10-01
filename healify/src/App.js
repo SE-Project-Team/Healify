1e539703d0b62f7703b278e4a740e52e134163ea
iamgowtami <cs19b010@iittp.ac.in>
2021-10-01 17:22:27 +0530

    dumy commit 2

--- healify/src/App.js
+++ healify/src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Home } from "./Components/Home";
+import {Signup} from "./Components/Signup";
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Signup/>
     </div>
   );
 }
