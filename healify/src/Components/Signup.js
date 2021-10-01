35232359813cbeaa7ba5b0ccc69f52613460ed83
iamgowtami <cs19b010@iittp.ac.in>
2021-10-01 19:40:59 +0530

    added signup page

--- healify/src/Components/Signup.js
+++ healify/src/Components/Signup.js
@@ -1,43 +1,41 @@
 import React from "react";
+import image1 from "../Assets/image1.png"
+import logo1 from "../Assets/mentalhealth_icon_round.png"
 import "./Signup.css";
 export const Signup = () => 
 {
   return (
-      <div class="container">
-    <div class="row">
-      <div class="col-lg-10 col-xl-9 mx-auto">
-        <div class="card flex-row my-5 border-0 shadow rounded-3 overflow-hidden">
-          <div class="card-img-left d-none d-md-flex">
-          </div>
-          <div class="card-body p-4 p-sm-5">
-            <h5 class="card-title text-center mb-5 fw-light fs-5">Welcome again</h5>
-            <form>
-
-              <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="floatingInputUsername" placeholder="myusername" required autofocus/>
-                <label for="floatingInputUsername">Username</label>
-              </div>
-
-              <div class="form-floating mb-3">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password"/>
-                <label for="floatingPassword">Password</label>
-              </div>
-
-              <div class="d-grid mb-2">
-                <button class="btn btn-lg btn-primary btn-login fw-bold " type="submit">Sign in</button>
-              </div>
-              <div class="d-grid mb-2">
-                <button class="btn btn-lg btn-google btn-login fw-bold text-uppercase" type="submit">
-                  <i class="fab fa-google me-2"></i> Sign in with Google
-                </button>
-              </div>
-
-              <a class="d-block text-center mt-2 small" href="#">Haven't an account? Sign Up</a>
-            </form>
-          </div>
+<div class="container px-4 py-5 mx-auto">
+    <div class="card card0">
+        <div class="d-flex flex-lg-row flex-column-reverse">
+            <div class="card card1">
+                <div class="row justify-content-center my-auto">
+                    <div class="col-md-8 col-10 my-5">
+                        <div class="row justify-content-center px-3 mb-3"> 
+                        <img id="logo" src={logo1} alt="logo1"/> </div>
+                        <h3 class="mb-5 text-center heading">Welcome again!</h3>
+                        <div class="form-group"> 
+                        <label class="form-control-label text-muted">Username</label>
+                         <input type="text" id="username" name="username" placeholder="Username" class="form-control"/> </div>
+                        <div class="form-group"> <label class="form-control-label text-muted">Password</label> 
+                        <input type="password" id="psw" name="psw" placeholder="Password" class="form-control"/> </div>
+                        <div class="row justify-content-center my-3 px-3"> <button class="btn-block btn-color">Login to Healify</button> </div>
+                        <div class="row justify-content-center my-2"> <a href="#"><small class="text-muted">Forgot Password?</small></a> </div>
+                    </div>
+                </div>
+                <div class="bottom text-center mb-5">
+                    <p href="#" class="sm-text mx-auto mb-3">Don't have an account?<button class="btn btn-white ml-2">Create new</button></p>
+                </div>
+            </div>
+            <div class="card card2">
+                <div class="my-auto mx-md-5 px-md-5 right">
+                    <div ><img class="rounded mx-auto d-block" src={image1} alt="healthgif" width="450px" height="280px" /></div><br /><br />
+                    <div><h3 class="text-white text-center mb-5">Healify</h3> </div>
+                    <div><small class="text-white">Dealing with mental health is what makes us human. But for a lot of us, it's a struggle to be open about it. Even if we are open about it, there is a global shortage of psychiatrists and lack of mental health care access in rural regions. In this context, we feel our website can serve as a viable tool to bridge the mental health treatment gap. Our app serves as a user feedback system for self-management and maintaining good mental health of every individual</small></div>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
+</div>
   );
 }
