ac431c6ef4ae79342ec4e211235757fcaeb1d455
Githin George <home@pop-os.localdomain>
2021-10-02 11:18:05 +0530

    App.js corrected

--- healify/src/App.js
+++ healify/src/App.js
@@ -1,28 +1,8 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Home } from "./Components/Home";
-import {Signup} from "./Components/Signup";
-import {Registration} from "./Components/Registration";
-function App() {
-  return (
-    <div className="App">
-      <Registration/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /> 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
-        
-      </header>
 import { Signup } from "./Components/Signup";
+import { Registration } from "./Components/Registration";
 import { Quiz } from "./Components/Quiz";
 import "./App.css";
 function App() {
@@ -30,6 +10,9 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
+          <Route path="/Registration">
+            <Registration />
+          </Route>
           <Route exact path="/">
             <Signup />
           </Route>
