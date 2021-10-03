2b270ee5d8b280905379d5d6166d768bb788c6cc
Githin George <home@pop-os.localdomain>
2021-10-03 23:33:51 +0530

    Minor Change

--- healify/src/Components/Memes.js
+++ healify/src/Components/Memes.js
@@ -14,31 +14,45 @@ export const Memes = () => {
     const jsonData = await response.json();
     setUserData(jsonData);
   };
-  
+
   return (
     <div
       id="carouselMultiItemExample"
-      className={memestyles.carousel+" "+"slide carousel-dark text-center"}
+      className={memestyles.carousel + " " + "slide carousel-dark text-center"}
       data-mdb-ride="carousel"
     >
-      <div className="d-flex justify-content-center mb-4">       
-      </div>
+      <div className="d-flex justify-content-center mb-4"></div>
       <div className="carousel-inner py-4">
         <div className="carousel-item active">
           <div className="container">
-                <div className="card ">
-                  <img src={userData.url} className="card-img-top" alt="..."/>
-                </div>
-                 <div className="card-body">
-                <a href="" className="btn btn-success btn-lg nsbtns" >Download&nbsp;&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
-            <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
-            <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
-            </svg></a>
-                <a href="" className={"btn btn-primary btn-lg"+" "+memestyles.nsbtns}>Next&nbsp;&gt;&gt;</a>
-              </div>
-              </div>
+            <div className="card ">
+              <img src={userData.url} className="card-img-top" alt="..." />
+            </div>
+            <div className="card-body">
+              <a href="" className="btn btn-success btn-lg nsbtns">
+                Download&nbsp;&nbsp;
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-download"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z" />
+                  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+                </svg>
+              </a>
+              <a
+                href=""
+                className={"btn btn-primary btn-lg" + " " + memestyles.nsbtns}
+              >
+                Next&nbsp;&gt;&gt;
+              </a>
             </div>
           </div>
         </div>
+      </div>
+    </div>
   );
 };
