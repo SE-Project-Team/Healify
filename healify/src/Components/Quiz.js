d49a318ea64b37c47806c26b449696954cf6e338
Githin George <home@pop-os.localdomain>
2021-10-03 15:11:30 +0530

    Quiz And Home Modules edited

--- healify/src/Components/Quiz.js
+++ healify/src/Components/Quiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import brainImg from "../Assets/brain.jpg";
+import logo from "../Assets/mentalhealth_icon_round.png";
 import styles from "./Quiz.module.css";
 import styles2 from "./Home.module.css";
 import { Questions } from "./Quiz/Questions";
@@ -24,51 +25,103 @@ export const Quiz = () => {
   };
   return (
     <>
-      <header>
+      {/* <header>
         <img className={styles2.logo} src={brainImg} />
         <h1 className={styles2.heading}>Healify</h1>
         <a className={styles2.login} href="">
           Log In
         </a>
+      </header> */}
+      <header className={styles2.header}>
+        <div className={styles2.threeLineBtn}>
+          <ul className={styles2.menuBtnCustom}>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+        <img className={styles2.logo} src={logo} />
+        <div>
+          <h1 className={styles2.heading}>Healify</h1>
+        </div>
+        <ul className={"nav " + styles2.settingsList}>
+          <li className={"nav-link"}>About</li>
+          <li className={"nav-link"}>Contact Us</li>
+          <li className={"nav-link"}>Profile</li>
+          <li className={styles2.navLoginBtn} id={styles2.idLoginBtn}>
+            Login
+          </li>
+          <li className={styles2.navLoginBtn} id={styles2.navSignUp}>
+            Sign Up
+          </li>
+        </ul>
       </header>
       <div className={styles.quizcontainer}>
         <div>
           <h1 className={styles.quizquestionBox}>{que.que}</h1>
-          <input
-            type="radio"
-            value={que.op1}
-            name={que}
-            onChange={handleChange}
-          />{" "}
-          {que.op1}
-          <input
-            type="radio"
-            value={que.op2}
-            name={que}
-            onChange={handleChange}
-          />{" "}
-          {que.op2}
-          <input
-            type="radio"
-            value={que.op3}
-            name={que}
-            onChange={handleChange}
-          />{" "}
-          {que.op3}
-          <input
-            type="radio"
-            value={que.op4}
-            name={que}
-            onChange={handleChange}
-          />{" "}
-          {que.op4}
-          <input
-            type="radio"
-            value={que.op4}
-            name={que}
-            onChange={handleChange}
-          />{" "}
-          {que.op5}
+          <form action="" className={styles.quizForm}>
+            <div>
+              <input
+                type="radio"
+                id={1}
+                value={que.op1}
+                name={que}
+                onChange={handleChange}
+              />{" "}
+              <label for={1} htmlFor={que.op1}>
+                {que.op1}
+              </label>
+            </div>
+
+            <div>
+              <input
+                id={2}
+                type="radio"
+                value={que.op2}
+                name={que}
+                onChange={handleChange}
+              />{" "}
+              <label for={2} htmlFor={que.op2}>
+                {que.op2}
+              </label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id={3}
+                value={que.op3}
+                name={que}
+                onChange={handleChange}
+              />{" "}
+              <label for={3} htmlFor={que.op3}>
+                {que.op3}
+              </label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id={4}
+                value={que.op4}
+                name={que}
+                onChange={handleChange}
+              />{" "}
+              <label for={4} htmlFor={que.op4}>
+                {que.op4}
+              </label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                value={que.op5}
+                id={5}
+                name={que}
+                onChange={handleChange}
+              />{" "}
+              <label for={5} htmlFor={que.op5}>
+                {que.op5}
+              </label>
+            </div>
+          </form>
         </div>
         <button className={styles.playBtn} onClick={newQue}>
           Next
