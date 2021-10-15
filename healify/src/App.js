8df83c36da108b74027829836ac4813aae473e23
SahithiRani <sahithirani2002@gmail.com>
2021-10-15 20:04:57 +0530

    Quiz.js updated

--- healify/src/App.js
+++ healify/src/App.js
@@ -7,7 +7,10 @@ import { Quiz } from "./Components/Quiz";
 import { Memes } from "./Components/Memes";
 import { Milestones } from "./Components/Milsestones";
 import { QuizLanding } from "./Components/QuizLanding";
+import { Questions } from "./Components/Quiz/Questions";
 import "./App.css";
+import { useState } from "react";
+
 function App() {
   return (
     <div className="App">
@@ -20,7 +23,7 @@ function App() {
             <Home />
           </Route>
           <Route path="/Quiz">
-            <Quiz />
+            <Quiz questions={Questions.slice(0, 79)} />
           </Route>
           <Route path="/Registration">
             <Registration />
@@ -36,6 +39,30 @@ function App() {
           </Route>
           <Route path="/QuizLanding">
             <QuizLanding />
+          </Route>
+          <Route path="/Quiz1">
+            <Quiz questions={Questions.slice(0, 12)} />
+          </Route>
+          <Route path="/Quiz2">
+            <Quiz questions={Questions.slice(12, 19)} />
+          </Route>
+          <Route path="/Quiz3">
+            <Quiz questions={Questions.slice(19, 27)} />
+          </Route>
+          <Route path="/Quiz4">
+            <Quiz questions={Questions.slice(27, 34)} />
+          </Route>
+          <Route path="/Quiz5">
+            <Quiz questions={Questions.slice(34, 43)} />
+          </Route>
+          <Route path="/Quiz6">
+            <Quiz questions={Questions.slice(43, 48)} />
+          </Route>
+          <Route path="/Quiz7">
+            <Quiz questions={Questions.slice(48, 69)} />
+          </Route>
+          <Route path="/Quiz8">
+            <Quiz questions={Questions.slice(69, 79)} />
           </Route>
         </Switch>
       </Router>
