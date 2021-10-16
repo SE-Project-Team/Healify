0295c0194bd044ff06d3bf65338c10c9a40b0743
SahithiRani <sahithirani2002@gmail.com>
2021-10-16 09:59:44 +0530

    Score Page Added

--- healify/src/App.js
+++ healify/src/App.js
@@ -7,7 +7,11 @@ import { Quiz } from "./Components/Quiz";
 import { Memes } from "./Components/Memes";
 import { Milestones } from "./Components/Milsestones";
 import { QuizLanding } from "./Components/QuizLanding";
-import { Questions } from "./Components/Quiz/QueCategory1";
+import { Questions1 } from "./Components/Quiz/QueCategory1";
+import { Questions2 } from "./Components/Quiz/QueCategory2";
+import { Questions3 } from "./Components/Quiz/QueCategory3";
+import { Questions4 } from "./Components/Quiz/QueCategory4";
+import { Score } from "./Components/Quiz/Score";
 import "./App.css";
 import { useState } from "react";
 
@@ -21,9 +25,6 @@ function App() {
           </Route>
           <Route path="/Home">
             <Home />
-          </Route>
-          <Route path="/Quiz">
-            <Quiz questions={Questions.slice(0, 79)} />
           </Route>
           <Route path="/Registration">
             <Registration />
@@ -41,28 +42,19 @@ function App() {
             <QuizLanding />
           </Route>
           <Route path="/Quiz1">
-            <Quiz questions={Questions.slice(0, 12)} />
+            <Quiz questions={Questions1} />
           </Route>
           <Route path="/Quiz2">
-            <Quiz questions={Questions.slice(12, 19)} />
+            <Quiz questions={Questions2} />
           </Route>
           <Route path="/Quiz3">
-            <Quiz questions={Questions.slice(19, 27)} />
+            <Quiz questions={Questions3} />
           </Route>
           <Route path="/Quiz4">
-            <Quiz questions={Questions.slice(27, 34)} />
+            <Quiz questions={Questions4} />
           </Route>
-          <Route path="/Quiz5">
-            <Quiz questions={Questions.slice(34, 43)} />
-          </Route>
-          <Route path="/Quiz6">
-            <Quiz questions={Questions.slice(43, 48)} />
-          </Route>
-          <Route path="/Quiz7">
-            <Quiz questions={Questions.slice(48, 69)} />
-          </Route>
-          <Route path="/Quiz8">
-            <Quiz questions={Questions.slice(69, 79)} />
+          <Route path="/Score">
+            <Score />
           </Route>
         </Switch>
       </Router>
--- healify/src/Components/Quiz.js
+++ healify/src/Components/Quiz.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react";
-import brainImg from "../Assets/brain.jpg";
 import logo from "../Assets/mentalhealth_icon_round.png";
 import styles from "./Quiz.module.css";
 import styles2 from "./Home.module.css";
 import SpecificQuestion from "./Quiz/Question";
 import { useState, useEffect } from "react";
+import { Score } from "./Quiz/Score";
 
 export const Quiz = (props) => {
   const Questions = props.questions;
-  const [option, setOption] = useState(-1);
+  const [option, setOption] = useState(0);
+  const [score, setScore] = useState(0);
   const handleChange = (e) => {
     const { name, value } = e.target;
     setOption(value);
     console.log(option);
+    setScore(() => score + parseInt(value));
   };
   //Counter for Number of Questions
   const [counter, setCounter] = useState(1);
@@ -21,64 +23,62 @@ export const Quiz = (props) => {
 
   const newQue = () => {
     setCounter(() => counter + 1);
+
     // counter will be updated
     const newQuestionSet = questionSet.filter((qn) => que.id !== qn.id);
     setQuestionSet(newQuestionSet);
     // state -> we expect it to be updated but its not
     let rand1 = Math.floor(Math.random() * newQuestionSet.length);
-
-    console.log(rand1);
     setQue(newQuestionSet[rand1] || Questions[0]);
-    if (counter == 5) {
-      //TODO Submit();
-      console.log("5 qns over");
-      setCounter(() => 0);
-    }
   };
-  useEffect(() => {
-    // runs after return/render
-    console.log(questionSet);
-    console.log(que.id);
-    console.log(que);
-  });
+
   return (
     <>
-      <header className={styles2.header}>
-        <div className={styles2.threeLineBtn}>
-          <ul className={styles2.menuBtnCustom}>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
-        </div>
-        <img className={styles2.logo} src={logo} />
-        <div>
-          <h1 className={styles2.heading}>Healify</h1>
-        </div>
-        <ul className={"nav " + styles2.settingsList}>
-          <li className={"nav-link"}>About</li>
-          <li className={"nav-link"}>Contact Us</li>
-          <li className={"nav-link"}>Profile</li>
-          <li className={styles2.navLoginBtn} id={styles2.idLoginBtn}>
-            Login
-          </li>
-          <li className={styles2.navLoginBtn} id={styles2.navSignUp}>
-            Sign Up
-          </li>
-        </ul>
-      </header>
-      <div className={styles.quizBody}>
-        <div className={styles.quizcontainer}>
-          <div>
-            <form action="" className={styles.quizForm}>
-              <SpecificQuestion qnObject={que} onChangeHandle={handleChange} />
-            </form>
+      {counter <= 5 ? (
+        <>
+          <header className={styles2.header}>
+            <div className={styles2.threeLineBtn}>
+              <ul className={styles2.menuBtnCustom}>
+                <li></li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+            <img className={styles2.logo} src={logo} />
+            <div>
+              <h1 className={styles2.heading}>Healify</h1>
+            </div>
+            <ul className={"nav " + styles2.settingsList}>
+              <li className={"nav-link"}>About</li>
+              <li className={"nav-link"}>Contact Us</li>
+              <li className={"nav-link"}>Profile</li>
+              <li className={styles2.navLoginBtn} id={styles2.idLoginBtn}>
+                Login
+              </li>
+              <li className={styles2.navLoginBtn} id={styles2.navSignUp}>
+                Sign Up
+              </li>
+            </ul>
+          </header>
+          <div className={styles.quizBody}>
+            <div className={styles.quizcontainer}>
+              <div>
+                <form action="" className={styles.quizForm}>
+                  <SpecificQuestion
+                    qnObject={que}
+                    onChangeHandle={handleChange}
+                  />
+                </form>
+              </div>
+              <button className={styles.playBtn} onClick={newQue}>
+                Next
+              </button>
+            </div>
           </div>
-          <button className={styles.playBtn} onClick={newQue}>
-            Next
-          </button>
-        </div>
-      </div>
+        </>
+      ) : (
+        <Score score={score} />
+      )}
     </>
   );
 };
