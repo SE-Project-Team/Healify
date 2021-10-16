1ea6f1ac422c8732296e16a7ae9f0e0af71bce90
iamgowtami <cs19b010@iittp.ac.in>
2021-10-16 21:59:51 +0530

    modified newsfinal file

--- healify/src/Components/News/NewsFinal.js
+++ healify/src/Components/News/NewsFinal.js
@@ -1,6 +1,6 @@
  import React, { createContext, useEffect, useState } from "react";
  import { NewsContextProvider } from "./NewsContext";
-// import News from "./News";
+ import News from "./News";
 
 function Newsfinal() {
 //   return (
