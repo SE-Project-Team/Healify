5b9ff9730a83530d56cfa87a81de360ab84fd51c
iamgowtami <cs19b010@iittp.ac.in>
2021-10-16 21:58:01 +0530

    modified news context file

--- healify/src/Components/News/NewsContext.js
+++ healify/src/Components/News/NewsContext.js
@@ -1,7 +1,7 @@
 // import React, { createContext, useEffect, useState } from "react";
 // import axios from "axios";
 
-// export const NewsContext = createContext();
+ export const NewsContext = createContext();
 
 export const NewsContextProvider = (props) => {
   const [data, setData] = useState();
