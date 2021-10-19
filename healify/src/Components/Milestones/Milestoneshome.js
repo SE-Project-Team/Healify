124502ac0e28bbe5d8ee0c976749c75025943740
roopasree123 <cs19b008@iittp.ac.in>
2021-10-19 14:11:56 +0530

    Milestones

--- healify/src/Components/Milestones/Milestoneshome.js
+++ healify/src/Components/Milestones/Milestoneshome.js
@@ -1,133 +1,78 @@
-import React from "react";
-import styles from "./Milestoneshome.module.css";
+import React , {useEffect, useState} from "react";
 import * as ReactBootStrap from "react-bootstrap";
-import { Link } from "react-router-dom";
-export const Milestoneshome = () => {
+import CreateTask from "../modals/CreateTask";
+import Card from "./Card";
+
+import  styles from "./MilestonesHome.module.css";
+import  "bootstrap/dist/css/bootstrap.min.css";
+export const MilestonesHome = () => {
+     const [modal, setModal] = useState(false);
+      const [taskList, setTaskList] = useState([])
+       useEffect(() => {
+        let arr = localStorage.getItem("taskList")
+       
+        if(arr){
+            let obj = JSON.parse(arr)
+            setTaskList(obj)
+        }
+    }, [])
+
+      const deleteTask = (index) => {
+        let tempList = taskList
+        tempList.splice(index, 1)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+     const toggle = () => setModal(!modal);
+     
+     const saveTask = (taskObj) => {
+        let tempList = taskList
+        tempList.push(taskObj)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(taskList)
+        setModal(false)
+        
+    }
+
   return (
-    <div>
-      <div>
-        <ReactBootStrap.Navbar
-          collapseOnSelect
-          expand="lg"
-          bg="dark"
-          variant="dark"
-        >
-          <ReactBootStrap.Container>
-            <ReactBootStrap.Navbar.Brand href="/home">
-              Milestones
-            </ReactBootStrap.Navbar.Brand>
-            <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
-              <ReactBootStrap.Nav className="me-auto">
-                <ReactBootStrap.Nav.Link href="/features"></ReactBootStrap.Nav.Link>
-                <ReactBootStrap.Nav.Link href="/pricing"></ReactBootStrap.Nav.Link>
-              </ReactBootStrap.Nav>
-              <ReactBootStrap.Nav>
-                <ReactBootStrap.Nav.Link href="/deets">
-                  Achievements
-                </ReactBootStrap.Nav.Link>
-                <ReactBootStrap.Nav.Link eventKey={2} href="/memes">
-                  Route Map
-                </ReactBootStrap.Nav.Link>
-              </ReactBootStrap.Nav>
-            </ReactBootStrap.Navbar.Collapse>
-          </ReactBootStrap.Container>
-        </ReactBootStrap.Navbar>
-      </div>
-      <h2 className="text-center">Welcome to Milestones</h2>
-      <section className={styles["details-card"]}>
-        <div className={styles.container}>
-          <div className="row">
-            <div className="col-md-4">
-              <div className={styles["card-content"]}>
-                <div className={styles["card-desc"]}>
-                  <h2>TITLE</h2>
-                  <hr />
-                  <h3>February 25 2019</h3>
-
-                  <p>Finally i achieved my dream goal</p>
-                  <a href="#" className={styles["btn-card"]}>
-                    Mark as done
-                  </a>
-                </div>
-              </div>
+     
+              <div>
+                   <ReactBootStrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <ReactBootStrap.Container>
+  <ReactBootStrap.Navbar.Brand href="#home">Milestones</ReactBootStrap.Navbar.Brand>
+  <ReactBootStrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <ReactBootStrap.Navbar.Collapse id="responsive-navbar-nav">
+    <ReactBootStrap.Nav className="me-auto">
+      <ReactBootStrap.Nav.Link href="#features"></ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link href="#pricing"></ReactBootStrap.Nav.Link>
+      
+    </ReactBootStrap.Nav>
+    <ReactBootStrap.Nav>
+         <ReactBootStrap.Nav.Link href="#deets">Home</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link href="#deets">Achievements</ReactBootStrap.Nav.Link>
+      <ReactBootStrap.Nav.Link eventKey={2} href="#memes">
+        Route Map
+      </ReactBootStrap.Nav.Link>
+    </ReactBootStrap.Nav>
+  </ReactBootStrap.Navbar.Collapse>
+  </ReactBootStrap.Container>
+</ReactBootStrap.Navbar>
+            <div className = {styles.header}>
+                <div className = "text-center">
+                <h3 className= "mt-"  >Milestones</h3>
+                < button className ="btn btn-primary mt-2"  onClick = {() => setModal(true)}>
+                    
+                    Create Task
+                    
+                    </button>
+        
+             </div>
+             </div>
+             <div className = {styles["task-container"]}>
+            {taskList && taskList.map((obj, index) => <Card taskObj = {obj} index = {index} deleteTask = {deleteTask}/>)}
             </div>
-            <div className="col-md-4">
-              <div className={styles["card-content"]}>
-                <div className={styles["card-desc"]}>
-                  <h2>TITLE</h2>
-                  <hr />
-                  <h3>January 19 1999</h3>
-
-                  <p>Finally i achieved my dream goal</p>
-                  <a href="#" className={styles["btn-card"]}>
-                    Mark as done
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className={styles["card-content"]}>
-                <div className={styles["card-desc"]}>
-                  <h2>TITLE</h2>
-                  <hr />
-                  <h3>December 15 2017</h3>
-
-                  <p>Finally i achieved my dream goal</p>
-                  <a href="#" className={styles["btn-card"]}>
-                    Mark as done
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-          <br />
-          <div className="row">
-            <div className="col-md-4">
-              <div className={styles["card-content"]}>
-                <div className={styles["card-desc"]}>
-                  <h2>TITLE</h2>
-                  <hr />
-                  <h3>February 25 2019</h3>
-
-                  <p>Finally i achieved my dream goal</p>
-                  <a href="#" className={styles["btn-card"]}>
-                    Mark as done
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className={styles["card-content"]}>
-                <div className={styles["card-desc"]}>
-                  <h2>TITLE</h2>
-                  <hr />
-                  <h3>January 19 1999</h3>
-
-                  <p>Finally i achieved my dream goal</p>
-                  <a href="#" className={styles["btn-card"]}>
-                    Mark as done
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className={styles["card-content"]}>
-                <div className={styles["card-desc"]}>
-                  <h2>TITLE</h2>
-                  <hr />
-                  <h3>December 15 2017</h3>
-
-                  <p>Finally i achieved my dream goal</p>
-                  <a href="#" className={styles["btn-card"]}>
-                    Mark as done
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
-  );
-};
+             <CreateTask toggle = {toggle} modal = {modal}  save = {saveTask}/>
+           </div>
+      );
+}
