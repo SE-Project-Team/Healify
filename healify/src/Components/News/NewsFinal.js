08afee702199de0a3f2af5d1556f43bc6abef833
iamgowtami <cs19b010@iittp.ac.in>
2021-10-24 19:56:02 +0530

    added header to articles

--- healify/src/Components/News/NewsFinal.js
+++ healify/src/Components/News/NewsFinal.js
@@ -1,10 +1,12 @@
  import React, { createContext, useEffect, useState } from "react";
  import { NewsContextProvider } from "./NewsContext";
  import News from "./News";
+import { Header } from "../Home/Header";
 
 function Newsfinal() {
    return (
      <NewsContextProvider>
+       <Header/>
        <News />
      </NewsContextProvider>
    );
