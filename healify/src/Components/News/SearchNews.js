b841ccda2fb5545ba9ae1f1187157ac6e6718606
iamgowtami <cs19b010@iittp.ac.in>
2021-10-29 14:07:31 +0530

    added search button

--- healify/src/Components/News/SearchNews.js
+++ healify/src/Components/News/SearchNews.js
@@ -6,21 +6,21 @@ const Search=({searchtext})=>{
         e.preventDefault()
         searchtext(text);
     }
-//    return(
-//        <>
-//        <form onSubmit={handleSubmit}>
-//        <div className={"container"+"  "+newsstyles.searchbody}>
-//         <div className="row height d-flex justify-content-center align-items-center">
-//         <div className="col-md-8">
-//             <div className={newsstyles.search}> 
-//             <i className={"fa fa-search"+"  "+newsstyles.fasearch}></i> 
-//             <input type="text" className={"form-control"+" "+newsstyles.searchinput} placeholder="Search news" onChange={(e)=>{settext(e.target.value)}}/> 
-//             <button type="submit" className={"btn btn-primary"+"  "+newsstyles.searchbtn}>Search</button> </div>
-//         </div>
-//        </div>
-//     </div> 
-//     </form>
-//     </>
-//    );  
-//    }  
+   return(
+       <>
+       <form onSubmit={handleSubmit}>
+       <div className={"container"+"  "+newsstyles.searchbody}>
+        <div className="row height d-flex justify-content-center align-items-center">
+        <div className="col-md-8">
+            <div className={newsstyles.search}> 
+            <i className={"fa fa-search"+"  "+newsstyles.fasearch}></i> 
+            <input type="text" className={"form-control"+" "+newsstyles.searchinput} placeholder="Search news" onChange={(e)=>{settext(e.target.value)}}/> 
+            <button type="submit" className={"btn btn-primary"+"  "+newsstyles.searchbtn}>Search</button> </div>
+        </div>
+       </div>
+    </div> 
+    </form>
+    </>
+   );  
+   }  
 export default Search;
