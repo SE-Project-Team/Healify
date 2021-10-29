ec26f042b99e0b5154525f54ee0ebd2379f3de76
Githin George <home@pop-os.localdomain>
2021-10-29 19:43:30 +0530

    Database schema updated with writeConcern

--- models/user.js
+++ models/user.js
@@ -1,124 +1,133 @@
 const mongoose = require("mongoose");
 
-const user = new mongoose.Schema({
-  username: {
-    type: String,
-    required: [true, "Name is required"],
-    maxlength: 24,
-    immutable: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-    minlength: [5, "password length should be atleast 8"],
-  },
-  email: {
-    type: String,
-    required: [true, "email is required"],
-  },
-  quizCat1: {
-    type: [
-      {
-        date: {
-          type: Date,
-          default: Date.now(),
-        },
-        score: {
-          type: Number,
-          required: [true, "Score is Required"],
-        },
-        remarks: {
-          type: String,
-          required: [true, "Remarks Not Mentioned"],
-        },
-      },
-    ],
-    default: [],
-  },
-  quizCat2: {
-    type: [
-      {
-        date: {
-          type: Date,
-          default: Date.now(),
-        },
-        score: {
-          type: Number,
-          required: [true, "Score is Required"],
-        },
-        remarks: {
-          type: String,
-          required: [true, "Remarks Not Mentioned"],
-        },
-      },
-    ],
-    default: [],
-  },
-  quizCat3: {
-    type: [
-      {
-        date: {
-          type: Date,
-          default: Date.now(),
-        },
-        score: {
-          type: Number,
-          required: [true, "Score is Required"],
-        },
-        remarks: {
-          type: String,
-          required: [true, "Remarks Not Mentioned"],
-        },
-      },
-    ],
-    default: [],
-  },
-  quizCat4: {
-    type: [
-      {
-        date: {
-          type: Date,
-          default: Date.now(),
-        },
-        score: {
-          type: Number,
-          required: [true, "Score is Required"],
-        },
-        remarks: {
-          type: String,
-          required: [true, "Remarks Not Mentioned"],
-          enum: {
-            values: ["Very Bad", "Bad", "Average", "Good", "Very Good"],
-            message: "${VALUE} is wrong",
+const user = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Name is required"],
+      maxlength: 24,
+      immutable: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [5, "password length should be atleast 8"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+    },
+    quizCat1: {
+      type: [
+        {
+          date: {
+            type: Date,
+            default: Date.now(),
+          },
+          score: {
+            type: Number,
+            required: [true, "Score is Required"],
+          },
+          remarks: {
+            type: String,
+            required: [true, "Remarks Not Mentioned"],
           },
         },
-      },
-    ],
-    default: [],
+      ],
+      default: [],
+    },
+    quizCat2: {
+      type: [
+        {
+          date: {
+            type: Date,
+            default: Date.now(),
+          },
+          score: {
+            type: Number,
+            required: [true, "Score is Required"],
+          },
+          remarks: {
+            type: String,
+            required: [true, "Remarks Not Mentioned"],
+          },
+        },
+      ],
+      default: [],
+    },
+    quizCat3: {
+      type: [
+        {
+          date: {
+            type: Date,
+            default: Date.now(),
+          },
+          score: {
+            type: Number,
+            required: [true, "Score is Required"],
+          },
+          remarks: {
+            type: String,
+            required: [true, "Remarks Not Mentioned"],
+          },
+        },
+      ],
+      default: [],
+    },
+    quizCat4: {
+      type: [
+        {
+          date: {
+            type: Date,
+            default: Date.now(),
+          },
+          score: {
+            type: Number,
+            required: [true, "Score is Required"],
+          },
+          remarks: {
+            type: String,
+            required: [true, "Remarks Not Mentioned"],
+            enum: {
+              values: ["Very Bad", "Bad", "Average", "Good", "Very Good"],
+              message: "${VALUE} is wrong",
+            },
+          },
+        },
+      ],
+      default: [],
+    },
+    milestones: {
+      type: [
+        {
+          title: {
+            type: String,
+            required: [true, "Title is Required"],
+          },
+          description: {
+            type: String,
+            required: [true, "Description is Required"],
+          },
+          completed: {
+            type: Boolean,
+            default: false,
+          },
+          targetDate: {
+            type: Date,
+            default: Date.now,
+          },
+        },
+      ],
+      default: [],
+    },
   },
-  milestones: {
-    type: [
-      {
-        title: {
-          type: String,
-          required: [true, "Title is Required"],
-        },
-        description: {
-          type: String,
-          required: [true, "Description is Required"],
-        },
-        completed: {
-          type: Boolean,
-          default: false,
-        },
-        targetDate: {
-          type: Date,
-          default: Date.now,
-        },
-      },
-    ],
-    default: [],
-  },
-});
+  {
+    writeConcern: {
+      w: "majority",
+      j: true,
+      wtimeout: 1000,
+    },
+  }
+);
 
 module.exports = mongoose.model("user", user);
